7a194ab4dd5f94c4292bf419fd99e71abd0ce626
Jonathan Nguyen <jnganguyen3@gmail.com>
2023-11-24 13:38:52 -0600

    changed configuration and converted to lua

---
+++ init.lua
@@ -0,0 +1,85 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Disables netrw, i.e. Exp, Hex
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local let set = vim.opt
+set.shiftwidth = 2
+set.tabstop = 2
+set.expandtab = true
+set.softtabstop = 2
+set.smarttab = true
+set.autoindent = true
+set.wrap = false
+set.number = true
+set.termguicolors = true
+
+require("lazy").setup('plugins')
+require("lualine").setup({
+  options = { fmt = string.lower },
+  sections = { lualine_a = {
+    {
+      'datetime',
+      style = '%H:%M'
+    }
+  }}
+})
+
+require('nvim-tree').setup {}
+vim.keymap.set('n', '<C-N>', ':NvimTreeToggle<CR>', {noremap=true, silent=true})
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = {"lua", "vim", "javascript", "go", "svelte", "sql"}, -- install parsers for all supported languages
+    --sync_install = false,
+    --ignore_install = { },
+    highlight = {
+      enable = true,
+      disable = { },
+    },
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-P>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.g.material_style = "darker"
+vim.cmd[[colorscheme tokyonight-night]]
+
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr) 
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp_signature_help'}
+  }
+})
+
+local lspconfig = require('lspconfig')
+lspconfig.tsserver.setup {}
+lspconfig.emmet_language_server.setup {}
+lspconfig.elixirls.setup {
+  cmd = {"/Users/wthunder/elixir-ls/elixir_ls/language_server.sh"}
+}
+lspconfig.svelte.setup {}
+lspconfig.lua_ls.setup {}
+lspconfig.gopls.setup {}
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+
---
+++ lua/plugins.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim', tag='0.1.2',
+    dependencies= {'nvim-lua/plenary.nvim'}
+  }, {
+    'nvim-orgmode/orgmode',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter', lazy = true },
+    },
+    event = 'VeryLazy',
+    config = function()
+      -- Load treesitter grammar for org
+      require('orgmode').setup_ts_grammar()
+
+      -- Setup treesitter
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { 'org' },
+        },
+        ensure_installed = { 'org' },
+      })
+
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      })
+    end,
+  }, {
+    "folke/tokyonight.nvim",
+    lazy=false,
+    priority=1000,
+    opts={}
+  }, {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy=false
+  }, {
+    'neovim/nvim-lspconfig'
+  }, {
+    'nvim-tree/nvim-tree.lua'
+  }, {
+    'nvim-treesitter/nvim-treesitter'
+  }, {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = true
+  }, {
+    "ellisonleao/gruvbox.nvim", priority=1000, config=true
+  }, {
+    "marko-cerovac/material.nvim"
+  },
+  {"VonHeikemen/lsp-zero.nvim", branch="v3.x"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"hrsh7th/nvim-cmp"},
+  {"hrsh7th/cmp-buffer"},
+  {"hrsh7th/cmp-path"},
+  {"L3MON4D3/LuaSnip"},
+  {"hrsh7th/cmp-nvim-lsp-signature-help"}
+}
